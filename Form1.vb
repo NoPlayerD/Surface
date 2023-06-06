d0c09e1b4f5cad3b67b6af1c72983943dcff6c51
NoPlayer <jadonredninja@gmail.com>
2023-06-06 15:46:12 +0300

    DataBase Check added
    Veri yolunu doğrulayan sub eklendi ve test edildi.

--- Form1.vb
+++ Form1.vb
@@ -1,6 +1,21 @@
-﻿Public Class Form1
+﻿Imports System.IO
+
+Public Class Form1
+    Dim DataPath As String = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\Surface"
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         'Çıkış butonu
         Application.Exit()
+    End Sub
+    Private Sub DataBase_Check()
+        'Veri yolunu doğrulama
+
+        If Not Directory.Exists(DataPath) Then
+            Directory.CreateDirectory(DataPath)
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DataBase_Check()
     End Sub
 End Class
